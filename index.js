6210dcf651bf6ba7a7b2f0e137a9259081d0c7e3
Juan Alorro <jralorro93@gmail.com>
2018-12-18 22:06:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var empty = [];
 
 function theBeatlesPlay (musicians, instruments) {
-  for (counter = 0; counter < 4; counter++) {
+  for (var counter = 0; counter < 4; counter++) {
     empty.push(`${musicians} plays ${instruments}`);
   }
   return empty;
